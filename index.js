2de9b2910cdc7e7ce4b92abf1a22be1b217b54a6
Félix Horro Pita <felix@corunet.com>
2020-05-16 11:57:58 +0200

    Create a meet!

--- index.js
+++ index.js
@@ -8,20 +8,7 @@ const keys = JSON.parse(fs.readFileSync(keyfile))
 const CALENDAR_ID = "bm28kde2rsnt1va19tp08adjh0@group.calendar.google.com"
 const TIMEZONE = "Europe/Madrid"
 
-function addACL(calendar, user) {
-	return calendar.acl.insert({
-		calendarId: CALENDAR_ID,
-		resource: {
-			role: "reader",
-			scope: {
-				type: "user",
-				value: user
-			}
-		}
-	})
-}
-
-async function main() {
+async function createEvent() {
 	const auth = google.auth.fromJSON(keys)
 	auth.scopes = ["https://www.googleapis.com/auth/calendar"]
 
@@ -30,23 +17,37 @@ async function main() {
 		auth
 	})
 
-	const now = new Date()
-	console.log(
-		await calendar.events.insert({
-			calendarId: CALENDAR_ID,
-			resource: {
-				start: {
-					dateTime: "2020-05-16T12:15:00",
-					timeZone: TIMEZONE
-				},
-				end: {
-					dateTime: "2020-05-16T12:45:00",
-					timeZone: TIMEZONE
-				},
-				summary: `A test - ${now.toISOString()}`
+	const now = new Date().toISOString()
+	const event = await calendar.events.insert({
+		calendarId: CALENDAR_ID,
+		conferenceDataVersion: 1,
+		resource: {
+			start: {
+				dateTime: "2020-05-16T12:15:00",
+				timeZone: TIMEZONE
+			},
+			end: {
+				dateTime: "2020-05-16T12:45:00",
+				timeZone: TIMEZONE
+			},
+			summary: `A test - ${now}`,
+			conferenceData: {
+				createRequest: {
+					requestId: now,
+					conferenceSolutionKey: {
+						type: "hangoutsMeet"
+					}
+				}
 			}
-		})
-	)
+		}
+	})
+
+	return event.data
+}
+
+async function main() {
+	const { hangoutLink } = await createEvent()
+	console.log(`Join the meeting at ${hangoutLink}`)
 }
 
 main().catch(console.error)
