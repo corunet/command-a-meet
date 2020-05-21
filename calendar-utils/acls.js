b9f56a80485633a345af8256e9c88b7d6ff89966
Félix Horro Pita <felix@corunet.com>
2020-05-21 17:28:47 +0200

    ACL: addUser & addGroup

--- calendar-utils/acls.js
+++ calendar-utils/acls.js
@@ -1,12 +1,20 @@
-exports.addACL = function (gCalendar, calendarId, user) {
+const addACL = function (gCalendar, calendarId, type, value) {
 	return gCalendar.acl.insert({
 		calendarId,
 		resource: {
 			role: "reader",
 			scope: {
-				type: "user",
-				value: user
+				type,
+				value
 			}
 		}
 	})
+}
+
+exports.addUser = function (gCalendar, calendarId, user) {
+	return addACL(gCalendar, calendarId, "user", user)
+}
+
+exports.addGroup = function (gCalendar, calendarId, group) {
+	return addACL(gCalendar, calendarId, "group", group)
 }
