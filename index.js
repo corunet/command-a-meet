bb3bda31e2c362fa7ca1e17fdccc4983d09a2870
Daniel López López <dlopez@corunet.com>
2020-05-21 17:33:12 +0200

    Use user's calendar

--- index.js
+++ index.js
@@ -12,19 +12,23 @@ const CALENDAR_ID = "bm28kde2rsnt1va19tp08adjh0@group.calendar.google.com"
 const keyfile = path.join(__dirname, "credentials.json")
 const keys = JSON.parse(fs.readFileSync(keyfile))
 
-const auth = google.auth.fromJSON(keys)
-auth.scopes = ["https://www.googleapis.com/auth/calendar"]
-const gCalendar = google.calendar({
-	version: "v3",
-	auth
-})
-
 app.get("/", async (req, res) => {
 	res.send("Mattermost - Google Meet integration")
 })
 app.post("/meet", async (req, res) => {
 	try {
-		const { hangoutLink } = await createEvent(gCalendar, CALENDAR_ID)
+		const auth = new google.auth.JWT(
+			keys.client_email,
+			null,
+			keys.private_key,
+			["https://www.googleapis.com/auth/calendar"],
+			req.query.user
+		)
+		const gCalendar = google.calendar({
+			version: "v3",
+			auth
+		})
+		const { hangoutLink } = await createEvent(gCalendar, "primary")
 		res.json({
 			text: `Join the meeting at ${hangoutLink}`,
 			response_type: "in_channel"
